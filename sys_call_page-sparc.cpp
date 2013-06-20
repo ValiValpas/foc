2915f3be0461b27a1c30c80dd63fbcfc6d98a28d
Johannes Schlatow <schlatow.ida.ing.tu-bs.de>
2013-06-20 17:39:42 +0200

    Sparc: implemented sys_call_page

--- sys_call_page-sparc.cpp
+++ sys_call_page-sparc.cpp
@@ -8,9 +8,22 @@ IMPLEMENTATION:
 #include "mem_layout.h"
 #include "kernel_task.h"
 #include "mem_space.h"
+#include "vmem_alloc.h"
+
 IMPLEMENT static
 void
 Sys_call_page::init()
 {
-  printf("%s fixme\n", __func__);
+  Mword *sys_calls = (Mword*)Mem_layout::Syscalls;
+  if (!Vmem_alloc::page_alloc(sys_calls,
+                              Vmem_alloc::NO_ZERO_FILL, Vmem_alloc::User))
+    panic("FIASCO: can't allocate system-call page.\n");
+
+  for (unsigned i = 0; i < Config::PAGE_SIZE; i += sizeof(Mword))
+    *(sys_calls++) = 0x91d02000; // ta 0
+
+  Kernel_task::kernel_task()
+    ->set_attributes(Virt_addr(Mem_layout::Syscalls),
+	             Page::Attr(Page::Rights::URX(), Page::Type::Normal(),
+		                Page::Kern::Global()));
 }
