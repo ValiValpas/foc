3ae864ed199c4f034e640440b8fbbe74724f2ac1
Johannes Schlatow <schlatow.ida.ing.tu-bs.de>
2013-08-10 10:00:36 +0200

    Sparc: implement sys_call_table

--- sys_call_page-sparc.cpp
+++ sys_call_page-sparc.cpp
@@ -19,13 +19,20 @@ Sys_call_page::init()
                               Vmem_alloc::NO_ZERO_FILL, Vmem_alloc::User))
     panic("FIASCO: can't allocate system-call page.\n");
 
-  for (unsigned i = 0; i < Config::PAGE_SIZE - 4*sizeof(Mword); i += sizeof(Mword))
+  for (unsigned i = 0; i < Config::PAGE_SIZE - 10*sizeof(Mword); i += sizeof(Mword))
     *(sys_calls++) = 0x91d02000; // ta 0
 
-  *(sys_calls+0) = 0x91d02010; // mem op: ta 0x10
-  *(sys_calls+1) = 0x91d02010; // invoke: ta 0x10
-  *(sys_calls+2) = 0x91d02000;
-  *(sys_calls+3) = 0x91d02000;
+  // write 10 entries that will be resolved by the sys_call_table
+  *(sys_calls+0) = 0x91d02010;
+  *(sys_calls+1) = 0x91d02010;
+  *(sys_calls+2) = 0x91d02010;
+  *(sys_calls+3) = 0x91d02010;
+  *(sys_calls+4) = 0x91d02010;
+  *(sys_calls+5) = 0x91d02010;
+  *(sys_calls+6) = 0x91d02010;
+  *(sys_calls+7) = 0x91d02010; // ipc invoke
+  *(sys_calls+8) = 0x91d02010;
+  *(sys_calls+9) = 0x91d02010;
 
   Kernel_task::kernel_task()
     ->set_attributes(Virt_addr(Mem_layout::Syscalls),
